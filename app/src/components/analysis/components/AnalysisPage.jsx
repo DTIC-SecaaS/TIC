8617da6f819a46e9b3208566c7ad6737963a63a5
Alejandra Oña <alejandra.onadelatorre@epn.edu.ec>
2024-10-26 18:39:15 -0500

    Creacion pagina de analisis completo y en progreso

--- app/src/components/analysis/components/AnalysisPage.jsx
+++ app/src/components/analysis/components/AnalysisPage.jsx
@@ -7,31 +7,49 @@ import DragAndDrop from "./DragAndDrop";
 import { analysisItems, getOptionTextByKey } from "../../../constants/consts";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import ArrowCircleLeftRoundedIcon from "@mui/icons-material/ArrowCircleLeftRounded";
 
-export default function AnalysisPage() {
+export default function AnalysisPage({ onBack }) {
   return (
     <Box
       sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#f4f6f8",
+        padding: 2,
         width: "100%",
       }}
     >
-      <Typography
-        component="h2"
-        variant="h6"
-        sx={{ mb: 2, textAlign: "center" }}
-      >
-        {getOptionTextByKey(analysisItems, "analysis")}
-      </Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onBack}
+          sx={{ mb: 2, display: "flex", alignItems: "center" }}
+        >
+          <ArrowCircleLeftRoundedIcon sx={{ marginRight: 1 }} />
+          Regresar
+        </Button>
+        <Typography
+          component="h2"
+          variant="h6"
+          sx={{ mb: 2, textAlign: "center" }}
+        >
+          {getOptionTextByKey(analysisItems, "analysis")}
+        </Typography>
 
-      <Grid container columns={12}>
-        <Grid size={{ sm: 12, md: 12, lg: 12 }}>
-          <DndProvider backend={HTML5Backend}>
-            <div style={{ padding: "20px" }}>
-              <DragAndDrop />
-            </div>
-          </DndProvider>
-        </Grid>
-        {/* <Grid
+        <Grid container columns={12}>
+          <Grid size={{ sm: 12, md: 12, lg: 12 }}>
+            <DndProvider backend={HTML5Backend}>
+              <div style={{ padding: "20px" }}>
+                <DragAndDrop />
+              </div>
+            </DndProvider>
+          </Grid>
+          {/* <Grid
           size={{ sm: 4, md: 4, lg: 3 }}
           sx={{
             backgroundColor: "rgba(90, 104, 133, 0.12)",
@@ -39,8 +57,13 @@ export default function AnalysisPage() {
         >
           hola
         </Grid> */}
-      </Grid>
-      <Copyright sx={{ my: 4 }} />
+        </Grid>
+      </Box>
+      <Copyright sx={{ marginTop: "auto", my: 4 }} />
     </Box>
   );
 }
+
+AnalysisPage.propTypes = {
+  onBack: PropTypes.func.isRequired,
+};
